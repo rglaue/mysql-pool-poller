3257d30f8d96e9635624b85feedefacccf8433b3
Russell Glaue <rglaue@gmail.com>
2007-12-03 05:53:51 +0000

    Cleaning up input options.

--- mpp/mpp-proxyadmin.pl
+++ mpp/mpp-proxyadmin.pl
@@ -35,12 +35,10 @@ sub usage (@) {
         usage: $0 [options]
 
     OPTIONS:
-    #NI# == "Not Implemented (yet) - Sorry."
     --cache-file=/path/to/cachefile
                 # The cache file the poller is manipulating the information for
     --pool=cfg_string   # The pool name only you wish to deal with
                         #  Example: --pool=mPoolA
-    #NI# --initialize=node      # Initialize a single node in MySQL Proxy accoring to MPP Pool
     --initialize-all    # Initialize all nodes in MySQL Proxy according to MPP Pool
     --set=node          # Set a single node in MySQL Proxy according to MPP Pool
     --set-all           # Set all MPP Pool nodes in MySQL Proxy accoriding to MPP Pool
@@ -52,6 +50,26 @@ sub usage (@) {
                 #---- rather than using what MPP has stored. However MPP will change it back to
                 #---- what it knows unless you turn off the Proxy-MPP communication.
     --weight=WEIGHT     # Initialize/Set the node's weight [0-99] (Used in MPP-as-Lua but not MPP Pool)
+    --show-nodes        # Show the nodes and their assignments
+    --help              # This help message
+
+    # MySQL connection options
+    --database          # MySQL Authorization database
+    --username          # MySQL Authorization username
+    --password          # MySQL Authorization password
+
+    # MySQL Proxy connection options
+    --proxyhost         # MySQL Proxy Host Address
+    --proxyport         # MySQL Proxy Port Number
+    --proxyuser         # MySQL Authorization username
+    --proxypass         # MySQL Authorization password
+
+HELP_EOF
+    #- Options to implement, or some version of them.
+    #NI# == "Not Implemented (yet) - Sorry."
+    #-
+    #NI# --initialize=node      # Initialize a single node in MySQL Proxy accoring to MPP Pool
+    #-
     #NI# --promote=node      # Promote the node up one level.
                         #  i.e. from UNKNOW to STANDBY, or STANDY to ACTIVE
                         #  Promoting from STANDBY to ACTIVE causes the current ACTIVE node to be demoted
@@ -60,37 +78,31 @@ sub usage (@) {
     #NI# --configure=atr:val # Configure MPP-as-Lua settings in the form of attribute:value
                         #  i.e. --configure=lbmethod:rr  - to set the load balance method to round robin
                         #  i.e. --configure=freeze:1  - to set the freeze feature on (same as --freeze)
-    --show-nodes        # Show the nodes and their assignments
+    #-
     #NI# --show-variables    # Show the configuration variables
-    --help              # This help message
-
-    # MySQL Proxy connection options
-    --host=hostname     # MySQL Proxy host name
-    --port              # MySQL Proxy port number
-    --database          # MySQL Authorization database
-    --username          # MySQL Authorization username
-    --password          # MySQL Authorization password
-
-HELP_EOF
+    #-
     $help   .= ("\n".$mesg."\n") if defined $mesg;
     return $help;
 }
 
 GetOptions( \%options,
         "cache-file=s", "pool=s",
-        "host=s",       "port=s",
         "database=s",   "username=s",   "password=s",
+        "proxyhost=s",  "proxyport=s",  "proxyuser=s", "proxypass=s",
         "initialize-all", "set-all", "show-nodes",
         "set=s", "type=s", "state=s", "status=s", "weight=s",
         "verbose", "help"
         );
 # Some defaults;
 $options{'cache-file'} ||= $failover_cachefile;
-$options{'host'} ||= "0.0.0.0";
-$options{'port'} ||= "3306";
-$options{'database'} ||= "test";
-$options{'username'} ||= "default_mysql_username";
-#$options{'password'} ||= "default_mysql_password";
+$options{'database'}   ||= "test";
+$options{'username'}   ||= "default_mysql_username";
+#$options{'password'}   ||= "default_mysql_password"; # uncomment to set a default
+$options{'proxyhost'}  ||= "0.0.0.0";
+$options{'proxyport'}  ||= "3306";
+$options{'proxyuser'}  ||= $options{'username'};
+#$options{'proxypass'}  ||= $options{'password'};
+
 
 if (! defined $options{'cache-file'}) {
     die usage("Cache file not provided!");
@@ -136,8 +148,8 @@ ERROR_EOF
 
 
 $failover   = init_failover_config($failover,$options{'pool'});
-$proxyhost  = new mysqlpool::host::mysqlproxy( host => $options{'host'}, port => $options{'port'},
-                                                username => $options{'username'}, password => $options{'password'} );
+$proxyhost  = new mysqlpool::host::mysqlproxy( host => $options{'proxyhost'}, port => $options{'proxyport'},
+                                                username => $options{'proxyuser'}, password => $options{'proxypass'} );
 
 if ((defined $options{'initialize-all'}) || (defined $options{'set-all'})) {
     if ((defined $options{'initialize-all'}) && ($options{'initialize-all'})) {
@@ -214,7 +226,7 @@ if ((defined $options{'initialize-all'}) || (defined $options{'set-all'})) {
         }
     }
 } elsif (defined $options{'show-nodes'}) {
-    my $a = $proxyhost->show("ALL");
+    my $a = $proxyhost->show("ALL") || die $proxyhost->errormsg(),"\n";
     my $indent = {};
     # We define this only to put the columns in a preferred order
     my @tablecol = qw (nodename group element value);
